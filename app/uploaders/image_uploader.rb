7e3255f4ae3882f69fb550c8a4d691a7edafd7ec
satoshi naruki <maptjgpatmpgg1113@gmail.com>
2021-08-25 17:46:19 +0900

    一覧画面の画像表示、画像リサイズ

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,14 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
+  include CarrierWave::MiniMagick
   storage :file
-  # storage :fog
-
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
+  process resize_to_fit: [100, 100]
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
